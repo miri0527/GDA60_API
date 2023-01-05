45ec16a91322e8143e271ff0fab23e9010582988
miri <miri6507@naver.com>
2023-01-05 17:40:26 +0900

    lastIndexOf,replace

--- src/com/iu/api1/string/StringStudy2.java
+++ src/com/iu/api1/string/StringStudy2.java
@@ -7,6 +7,21 @@ public class StringStudy2 {
 		int idx = email.lastIndexOf("@");
 		System.out.println(idx);
 		
+		System.out.println(email.length());
+		
+		for(int i=0; i<email.length(); i++) {
+			System.out.print(email.charAt(i) + " ");
+		}
+		
+		String str = "a";
+		String str2 = " a";
+		//String str3 = null; //에러
+		System.out.println("\nSTR: " + str.length());
+		
+		System.out.println("Before Email : " + email);
+		String a = email.replace("@", "_");
+		System.out.println("After Email: " + a);
+		
 	}
 
 }

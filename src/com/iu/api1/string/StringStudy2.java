14a0d7b4302675b318aec2c0fedd9fb96e65bbc9
miri <miri6507@naver.com>
2023-01-05 17:13:45 +0900

    lastindexOf

---
+++ src/com/iu/api1/string/StringStudy2.java
@@ -0,0 +1,12 @@
+package com.iu.api1.string;
+
+public class StringStudy2 {
+
+	public static void main(String[] args) {
+		String email = "a@bc@123@gmail.com";
+		int idx = email.lastIndexOf("@");
+		System.out.println(idx);
+		
+	}
+
+}
